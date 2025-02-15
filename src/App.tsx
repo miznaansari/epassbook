b11356c07ad9b7415a84a6f493f39d6e02e49201
miznaansari <miznaansari@gmail.com>
2025-02-15 12:22:15 +0530

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,38 @@
-
-import './App.css'
-import Login from './components/Login'
-import SectionA from './components/SectionA'
-import SectionB from './components/SectionB'
-import SectionC from './components/SectionC'
-import Signup from './components/Signup'
-import AuthState from './context/AuthState'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from './components/Login';
+import SectionA from './components/SectionA';
+import SectionB from './components/SectionB';
+import SectionC from './components/SectionC';
+import Signup from './components/Signup';
+import AuthState from './context/AuthState';
 
 function App() {
-
   return (
-    <>
-    <AuthState >
-    <Signup />
-
+    <AuthState>
+      <Router>
+        <Routes>
+          {/* Show Login on "/login" */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Signup />} />
+          
+          {/* Default route: Show SectionA, SectionB, SectionC */}
+          <Route path="/*" element={<MainSections />} />
+        </Routes>
+      </Router>
     </AuthState>
-    {/* <Login /> */}
-      {/* <SectionA />
-     <SectionB />
-     <SectionC />  */}
-     
-    </>
-  )
+  );
 }
 
-export default App
+// Separate component to group sections
+const MainSections = () => {
+  return (
+    <>
+      <SectionA />
+      <SectionB />
+      <SectionC />
+    </>
+  );
+};
+
+export default App;
