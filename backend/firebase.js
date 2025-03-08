4f876e0d5a8d579661942d23b3a6186f18df9b37
miznaansari <miznaansari@gmail.com>
2025-03-08 14:38:48 +0530

    Sign with google

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,8 +1,15 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-admin.json");
+require("dotenv").config();
+
+const firebaseCredentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: firebaseCredentials.project_id,
+    clientEmail: firebaseCredentials.client_email,
+    privateKey: firebaseCredentials.private_key.replace(/\\n/g, '\n'),
+  }),
 });
+
 
 module.exports = admin;
